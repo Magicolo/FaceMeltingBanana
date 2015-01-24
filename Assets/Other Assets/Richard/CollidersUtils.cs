a94b1b3362c0967837344eb74a16a64e14f82099
Richard Rail <rail.rich@gmail.com>
2015-01-24 01:24:15 -0500

    RoomLoader

---
+++ Assets/Other Assets/Richard/CollidersUtils.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollidersUtils  {
+
+	public static Collider2D getCollider(Vector3 rayFrom, Vector3 rayDirection, float maxDistance){
+		rayFrom.z = 0;
+		RaycastHit2D hit = Physics2D.Raycast(rayFrom, rayDirection);
+		if (hit.collider != null) {
+			float realDistance = Vector3.Distance(rayFrom,hit.collider.transform.position);
+			if(realDistance <= maxDistance){
+				return hit.collider;
+			}
+		}
+		return null;
+	}
+}
