bebc411144597b1d157892b6b199632255d4bba8
Richard Rail <rail.rich@gmail.com>
2015-01-24 18:41:46 -0500

    asd

---
+++ Assets/Resources/Scripts/Puzzles/FloorButtonPuzzle/PuzzleFloorButtonWin.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using Magicolo;
+
+public class PuzzleFloorButtonWin : State {
+
+	PuzzleFloorButton Layer {
+		get { return ((PuzzleFloorButton)layer); }
+	}
+	
+	public override void OnEnter() {
+		RoomFlowManager.instance.goToNextRoom();
+	}
+}
---
+++ Assets/Resources/Scripts/Tiles/FloorButton/FloorButtonIdle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using Magicolo;
+
+public class FloorButtonIdle : State {
+
+	FloorButton Layer {
+    	get { return ((FloorButton)layer); }
+    }
+    
+	public override void OnEnter() {
+		
+	}
+	
+	public override void TriggerEnter(Collider collision) {
+		if (collision.gameObject.name == "Player") {
+			SwitchState<FloorButtonPlayerOn>();
+		}
+	}
+}
